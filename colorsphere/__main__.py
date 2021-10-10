2efae85075ac41cb144fc509a03d22229dede2d2
Tom Ritchford <tom@swirly.com>
2021-10-10 16:13:15 +0200

    Use blessed for the demo

--- colorsphere/__main__.py
+++ colorsphere/__main__.py
@@ -1,16 +1,30 @@
 from . colorsphere import ColorPicker
 from matplotlib import pyplot
+from blessed import Terminal
+from functools import wraps
 
 
-def make_callback(name):
-    return lambda *a, **ka: print(name, a, ka)
+
+def print_at(name, term, items, x=0, y=0):
+    if items:
+        with term.location(x, y):
+            print(name, ', '.join(f'{i:+.3f}' for i in items))
 
 
-def main():
-    cp = ColorPicker(make_callback('click'), make_callback('move'))
+def main(term):
+    def click(rgb, event):
+        print_at('Click', term, rgb, 0, 1)
+
+
+    def move(rgb, event):
+        print_at('Move ', term, rgb)
+
+
+    print(term.home + term.clear)
+    cp = ColorPicker(click, move)
     pyplot.ioff()
     pyplot.show()
 
 
 if __name__ == '__main__':
-    main()
+    main(Terminal())
